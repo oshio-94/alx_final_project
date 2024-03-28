27b553447dd9389cb453e51c56f95552a9c949a4
John Ikhanoba <oshioi@yahoo.co.uk>
2024-03-28 10:56:17 +0100

    add database connection

--- admin_page.php
+++ admin_page.php
@@ -4,11 +4,6 @@
 
 if(isset($_POST['add_product'])){
 
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-   $product_image = $_FILES['product_image']['name'];
-   $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
-   $product_image_folder = 'uploaded_files/'.$product_image;
 
    if(empty($product_name) || empty($product_price) || empty($product_image)){
       $message[] = 'please fill out all';
